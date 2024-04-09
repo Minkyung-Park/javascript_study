1bfb46a06b06521570285547d22df48d52cb326d
Minkyung-Park <mp.minkyung@gmail.com>
2024-04-09 12:37:03 +0900

    if문 switch문

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-// 원의 넓이 구하기
-let r = 8;
-let area;
-const pi = 3.14;
+let num = 9;
 
-area = r * r * pi;
-console.log(`반지름: ${r}`);
-console.log(`원의 넓이: ${area}`);
+if (num % 2 == 0) {
+  console.log(`${num}은 짝수입니다`);
+} else {
+  console.log(`${num}은 홀수입니다`);
+}
+
+// 70점 이상이면 합격, 그렇지 않으면 불합격
+let score = 65;
+let result = "";
+
+if (score >= 70) {
+  result = "합격";
+} else {
+  result = "불합격";
+}
+console.log(result);
+
+let score = 65;
+let result = "";
+
+if (score >= 70) {
+  result = "합격";
+} else {
+  result = "불합격";
+}
+console.log(result);
