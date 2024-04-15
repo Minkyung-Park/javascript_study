7afff89df328f33de2ef5b7c506601e6786e6959
Minkyung-Park <mp.minkyung@gmail.com>
2024-04-15 10:34:42 +0900

    Learning

--- js/array.js
+++ js/array.js
@@ -1,6 +1,12 @@
-const animals = ["사자", "호랑이", "사슴", "펭귄", "여우", "도마뱀"];
+const numbers = [1, 2, 3, 4, 5];
 
-// 인덱스 1부터 인덱스4 사이(4는 포함하지 않음)의 요소
-const result = animals.slice(1, 4);
+// 함수를 파라미터로 넘겨주는거 -> 콜백 함수
 
-console.log(result); // false
+// map() 메서드는 forEach() 메서드와 달리 매개 변수로 사용되는 함수에서 값을 반환하는 return이 존재
+// map() 메서드는 원본의 배열을 그대로 두고 새로운 배열을 생성
+// map() 메서드에 매개 변수로 설정된 익명 함수를 실행하여 얻어진 요소로 구성된 새로운 배열을 반환
+let arr = numbers.map((num) => num * num);
+console.log(numbers);
+console.log(arr);
+
+document.getElementById("show").innerHTML = arr;
